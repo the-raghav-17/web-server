bb3f434f1916e29af2ca854d537cb20b2d854f8e
Raghav Sharma <code.practice17@gmail.com>
2026-06-23 20:47:07 +0530

    Parser: Refactor request line parser

--- src/parser/parser.cpp
+++ src/parser/parser.cpp
@@ -4,49 +4,61 @@
 #include <string>
 
 
-Parser::Parse_request(const std::string& msg) const
+Http::Request Parser::parse_request(const std::string& msg) const
 {
     Http::Request request{};
 
-    std::vector<std::string> parsed_request{};
-    std::size_t i{ 0 };
-
-    // Parse Method and resource path
-    for (int j = 0; j < 2; j++) {
-        auto found{ msg.find(' ', i) };
-        if (found == std::string::npos) {
-            // TODO: Handle request parsing error
-            request.is_valid = false;
-        }
-        parsed_request.emplace_back(msg, i, found-i);
-        i = found + 1;
+    if (parse_request_line(request, msg) == -1) {
+        return request;
     }
 
-    // Parse http version
-    parsed_request.emplace_back(msg, i);
+    return request;
+}
+
+
+int parse_request_line(Http::Request& request, const std::string& request_line) const
+{
+    using TOK_COUNT = 3;
+
+    auto parsed_request{ Parse_helper::tokeize_string(request_line, ' ') };
+    if (parsed_request.size() != TOK_COUNT) {
+        request.is_valid = false;
+        return -1;
+    }
+
+    using METHOD_INDEX   = 0;
+    using RESOURCE_INDEX = 1;
+    using VERSION_INDEX  = 2;
 
     // Get the method type
-    auto& method{ parsed_request.at(0) };
-    if (method == std::string("GET")) {
+    auto& method{ parsed_request.at(METHOD_INDEX) };
+    if (method == std::string{ "GET" }) {
         request.method = Http::Method::GET;
-    } else if (method == std::string("HEAD")) {
+    }
+    else if (method == std::string{ "HEAD" }) {
         request.method = Http::Method::HEAD;
-    } else if (method == std::string("POST")) {
+    }
+    else if (method == std::string{ "POST" }) {
         request.method = Http::Method::POST;
-    } else {
+    }
+    else {
         request.is_valid = false;
+        return -1;
     }
 
     // Get the resource path
-    request.path = std::move(parsed_request.at(1));
+    request.path = std::move(parsed_request.at(RESOURCE_INDEX));
+    // TODO: Add check for resource path syntax
 
     // Get the http version
-    auto& version{ parsed_request.at(2) };
-    if (version == std::string("HTTP/1.0\n")) {
+    auto& version{ parsed_request.at(VERSION_INDEX) };
+    if (version == std::string{ "HTTP/1.0" }) {
         request.version = Http::Version::V1_0;
-    } else if (version == std::string("HTTP/1.1\n")) {
+    }
+    else if (version == std::string{ "HTTP/1.1" }) {
         request.version = Http::Version::V1_1;
-    } else {
+    }
+    else {
         request.is_valid = false;
     }
 
