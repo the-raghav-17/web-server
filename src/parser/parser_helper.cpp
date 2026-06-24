71c8f6da56d704d5e3f484224cecf572cb9d8b7c
Raghav Sharma <code.practice17@gmail.com>
2026-06-24 20:51:21 +0530

    Parser: Add string tokenizer

--- src/parser/parser_helper.cpp
+++ src/parser/parser_helper.cpp
@@ -7,5 +7,24 @@
 std::vector<std::string>
 Parser_helper::tokenize_string(const std::string& str, const std::string& delim)
 {
-    // TODO: tokenize_string
+    std::vector<std::string> out{};
+    std::size_t i{ 0 };
+
+    for (auto found = str.find(delim, 0);
+        found != std::string::npos;
+        found = str.find(delim, i)) {
+
+        // We're on the delim itself, so don't add it to token
+        if (str.substr(i, delim.size()) != delim) {
+            out.emplace_back(str, i, found-i);
+        }
+
+        i = found + delim.size();
+    }
+    // If we go across the string (when delim is at end)
+    if (i < str.size()) {
+        out.emplace_back(str, i);
+    }
+
+    return out;
 }
