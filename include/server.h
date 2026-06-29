a2b914f3bb048b67e4d65ce1e5436a882adfc723
Raghav Sharma <code.practice17@gmail.com>
2026-06-29 17:06:12 +0530

    Server: Defined root path and server exception

--- include/server.h
+++ include/server.h
@@ -19,15 +19,35 @@
 #include "http.h"
 #include "parser.h"
 
+#include <filesystem>
+
+
+class Server_error : public std::runtime_error
+{
+public:
+    Server_error(const std::string& msg):
+        runtime_error{ msg }
+    {}
+};
+
 
 class Server
 {
 public:
     // TODO: Add server constructor that enables signal handling andd initialized invariants
+    Server(const std::string& path):
+        m_root_dir{ path }
+    {
+        if (!std::filesystem::exists(m_root_dir)) {
+            throw Server_error{ path + ": No such path exists" };
+        }
+    }
+
     void start() const;
 
 private:
-    Parser m_parser{};
+    std::filesystem::path m_root_dir{};
+    Parser                m_parser{};
 
     void handle_client(Socket& remote_socket) const;
     void print_request_details(const Http::Request& msg_request) const;
--- src/main.cpp
+++ src/main.cpp
@@ -18,13 +18,34 @@
 #include <iostream>
 
 
-int main()
+int main(int argc, char* argv[])
 {
+    // FIX: Giving root path argument
+
+    const std::string root_path{ "./public/" };
+    //
+    // if (argc > 2) {
+    //     std::cerr << "Usage: " << argv[0] << " [root-path]\n";
+    //     return 1;
+    // }
+    // else if (argc == 2) {
+    //     root_path += std::string(argv[1]);
+    // }
+    // else {
+    //     root_path += std::string("./public/");
+    // }
+
     try {
-        Server server{};
+        Server server{ root_path };
         server.start();
-    } catch(...) {
-        std::cerr << "main: Uncaught execption recieved...\n";
+    }
+    catch (Server_error& err) {
+        std::cerr << "Error: Server: " + std::string{ err.what() } << '\n';
+        return 1;
+    }
+    catch (...) {
+        std::cerr << "Some unknown exception caught...\n";
+        return 1;
     }
 
     return 0;
