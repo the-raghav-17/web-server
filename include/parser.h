54869845f0eaa371926f25cab12b67f5611492a0
Raghav Sharma <code.practice17@gmail.com>
2026-07-10 22:34:30 +0530

    Parser: Make parser a namespace instead of class

--- include/parser.h
+++ include/parser.h
@@ -8,16 +8,16 @@
 #include <vector>
 
 
-class Parser
+namespace Parser
 {
-public:
-    Http::Request parse_request(const std::string& request_msg) const;
-
-    static std::vector<std::string>
+    std::vector<std::string>
         tokenize_string(const std::string& str, const std::string& delim);
 
-private:
-    int parse_request_line(const std::string& request_line, Http::Request& request) const;
+    Http::Request
+        parse_request(const std::string& request_msg);
+
+    int parse_request_line(const std::string& request_line,
+                           Http::Request& request);
 };
 
 
--- src/parser/parser.cpp
+++ src/parser/parser.cpp
@@ -4,14 +4,16 @@
 #include <string>
 
 
-Http::Request Parser::parse_request(const std::string& request_msg) const
+Http::Request Parser::parse_request(const std::string& request_msg)
 {
-    auto parsed_request{ tokenize_string(request_msg, "\r\n") };
-    constexpr int REQUEST_LINE_INDEX{ 0 };
-
     Http::Request request{};
+    request.is_valid = false;
 
-    const std::string& request_line{ parsed_request.at(REQUEST_LINE_INDEX) };
+    // Tokenize the request message into different lines
+    auto request_parts{ tokenize_string(request_msg, "\r\n") };
+
+    // Request line comes first
+    const std::string& request_line{ request_parts.front() };
     if (parse_request_line(request_line, request) == -1) {
         return request;
     }
@@ -22,22 +24,23 @@ Http::Request Parser::parse_request(const std::string& request_msg) const
 }
 
 
-int Parser::parse_request_line(const std::string& request_line, Http::Request& request) const
+int Parser::parse_request_line(const std::string& request_line, Http::Request& request)
 {
-    constexpr std::size_t TOK_COUNT{ 3 };
+    constexpr std::size_t PARTS_COUNT{ 3 };
 
-    auto parsed_request_line{ tokenize_string(request_line, " ") };
-    if (parsed_request_line.size() != TOK_COUNT) {
+    // Tokenize the request line into parts
+    auto request_line_parts{ tokenize_string(request_line, " ") };
+    if (request_line_parts.size() != PARTS_COUNT) {
         request.is_valid = false;
         return -1;
     }
 
     constexpr std::size_t METHOD_INDEX{ 0 };
-    constexpr std::size_t RESOURCE_INDEX{ 1 };
+    constexpr std::size_t PATH_INDEX{ 1 };
     constexpr std::size_t VERSION_INDEX{ 2 };
 
     // Get the method type
-    auto& method{ parsed_request_line.at(METHOD_INDEX) };
+    const auto& method{ request_line_parts.at(METHOD_INDEX) };
     if (method == std::string{ "GET" }) {
         request.method = Http::Method::GET;
     }
@@ -50,15 +53,14 @@ int Parser::parse_request_line(const std::string& request_line, Http::Request& r
     }
 
     // Get the resource path
-    request.path = std::move(parsed_request_line.at(RESOURCE_INDEX));
-    // TODO: Add check for resource path syntax
+    request.path = std::move(request_line_parts.at(PATH_INDEX));
 
     // Get the http version
-    auto& version{ parsed_request_line.at(VERSION_INDEX) };
-    if (version == std::string{ "HTTP/1.0" }) {
+    const auto& version{ request_line_parts.at(VERSION_INDEX) };
+    if (version == "HTTP/1.0") {
         request.version = Http::Version::V1_0;
     }
-    else if (version == std::string{ "HTTP/1.1" }) {
+    else if (version == "HTTP/1.1") {
         request.version = Http::Version::V1_1;
     }
     else {
