149f6b1d3f39e9bb58ddc321eefbb20e90f9d2e5
Raghav Sharma <code.practice17@gmail.com>
2026-07-17 10:32:19 +0530

    Config: Define basic layout for parsing

--- src/config.cpp
+++ src/config.cpp
@@ -1,3 +1,16 @@
+/*
+ * SPDX-License-Identifier: GNU GPLv3
+ * Copyright (c) 2026 Raghav Sharma
+ * GitHub: https://github.com/the-raghav-17
+ */
+
+/*
+ * config.cpp
+ *
+ * Implementation of config functions
+ */
+
+
 #include "config.h"
 
 #include <string>
@@ -9,7 +22,72 @@
 
 
 Config::Type
-Config::parse_config_file(const std::string& file)
+Config::parse_config_file(const std::string& file_path)
 {
-    // TODO: Config parsing
+    // TODO: Logic for file handling
+    // ...
+    // Assume config file was opened as file_contents
+
+    // Divide the config into lines
+    auto config_parts = Parser::tokenize_string(file_contents, "\r\n");
+    if (config_parts.size() > 3) {
+        // TODO: Throw error: Excess parameter count... Expected at most 3 got config_parts.size()
+    }
+
+    // Set to see if a parameter appears more than once
+    std::unordered_set<std::string> set{};
+
+    // Now process each line
+    for (const auto& config_line : config_parts) {
+        // Divide line based on tokens separated by space character
+        auto line_parts = Parser::tokenize_string(config_line, " ");
+
+        // Each line must consist of exactly three tokens:
+        // 'parameter' '=' 'value'
+        if (line_parts.size() != 3) {
+            // TODO: Throw error: Excess tokens
+        }
+
+        // For now only 3 parameters are defined:
+        // - path: root directory which consists all HTTP content
+        // - threads: number of threads in the threadpool
+        // - queue: size of waiting queue while listening
+        //
+        // Ex:
+        // path = ./content
+        // threads = 10
+        // queue = 7
+        
+        if (line_parts.at(1) != "=") {
+            // TODO: Throw error: Expected "=" got line_parts.at(1)
+        }
+
+        const auto param{ line_parts.at(0) };
+        const auto value{ line_parts.at(2) };
+
+        // Make sure the parameter only exists once
+        if (set.count(param) > 0) {
+            // TODO: Throw error: Param path defined twice
+        }
+        set.insert(param);
+        
+        if (param == "path") {
+            config.root_path = value;
+        }
+        else if (param == "threads") {
+            if (!is_int(value)) {
+                // TODO: Throw error: param threads expected int
+            }
+            config.threads = value;
+        }
+        else if (param == "queue") {
+            if (!is_int(value)) {
+                // TODO: Throw error: param queue expected int
+            }
+            config.queue = value;
+        }
+        else {
+            // TODO: Throw error: Unexpected parameter
+        }
+    }
 }
